8eb36ef4a18b85dfed375de6f911c5651a6d8196
@wahjoe.eko.n <wahjoe.eko.n@gmail.com>
2020-07-28 01:44:43 +0700

    + added AppNavigator.prefab - rename scene 'Tentang' with 'Peneliti' - MainMenu Functionality: testing button

--- Hydasa/Assets/Scripts/AppNavigation.cs
+++ Hydasa/Assets/Scripts/AppNavigation.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AppNavigation : MonoBehaviour
 {
     public void ExitApp()
     {
         Application.Quit();
+    }
+
+    public void OpenScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
