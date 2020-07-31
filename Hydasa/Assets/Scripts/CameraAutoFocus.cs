6fc0b11cf02f468275a39d5e9aa85cee9b4a7a97
@wahjoe.eko.n <wahjoe.eko.n@gmail.com>
2020-07-31 14:44:52 +0700

    + added camera auto focus script - implemented auto focus to AR Camera

---
+++ Hydasa/Assets/Scripts/CameraAutoFocus.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class CameraAutoFocus : MonoBehaviour {
+    private bool mVuforiaStarted = false;
+
+    void Start () 
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+
+        if (vuforia != null)
+            vuforia.RegisterVuforiaStartedCallback(StartAfterVuforia);
+    }
+
+    private void StartAfterVuforia()
+    {
+        mVuforiaStarted = true;
+        SetAutofocus();
+    }
+
+    void OnApplicationPause(bool pause)
+    {
+        if (!pause)
+        {
+            // App resumed
+            if (mVuforiaStarted)
+            {
+                // App resumed and vuforia already started
+                SetAutofocus(); // This is done because some android devices lose the auto focus after resume
+                // this was a bug in vuforia 4 and 5. I haven't checked 6, but the code is harmless anyway
+            }
+        }
+    }
+
+    private void SetAutofocus()
+    {
+        if (CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO))
+        {
+            Debug.Log("wahyu_debug: Autofocus set");
+        }
+        else
+        {
+            Debug.Log("wahyu_debug: this device doesn't support auto focus");
+        }
+    }
+}
